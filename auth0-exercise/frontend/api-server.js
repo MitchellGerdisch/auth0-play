e677fadc4cd08368ff2004a9e417ff427cf6c24d
Gerdisch <github@jesslex.com>
2020-08-13 21:21:44 -0500

    getting closer

--- auth0-exercise/frontend/api-server.js
+++ auth0-exercise/frontend/api-server.js
@@ -4,15 +4,18 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
-const authConfig = require("./src/auth_config.json");
 
 const app = express();
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+const serviceUrl = process.env.REACT_APP_SERVICE_URL;
+
 const port = process.env.API_PORT || 3001;
 const appPort = process.env.SERVER_PORT || 3000;
-const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
+const appOrigin = serviceUrl || `http://localhost:${appPort}`;
 
-if (!authConfig.domain || !authConfig.audience) {
+if (!domain || !audience) {
   throw new Error(
     "Please make sure that auth_config.json is in place and populated"
   );
@@ -27,11 +30,11 @@ const checkJwt = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+    jwksUri: `https://${domain}/.well-known/jwks.json`
   }),
 
-  audience: authConfig.audience,
-  issuer: `https://${authConfig.domain}/`,
+  audience: audience,
+  issuer: `https://${domain}/`,
   algorithm: ["RS256"]
 });
 
--- auth0-exercise/frontend/src/OrderForm.js
+++ auth0-exercise/frontend/src/OrderForm.js
@@ -1,35 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import createAuth0Client from '@auth0/auth0-spa-js';
+
 
 export function OrderForm(props) {
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [salutation, setSalutation] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const {
+    user,
+    isAuthenticated,
+  } = useAuth0()
+
+  const [accessToken, setToken] = useState("");
+  
+    useEffect(() => {
+      async function fetchData() {
+        if (isAuthenticated) {
+          const auth0 = await createAuth0Client({
+            domain: process.env.REACT_APP_AUTH0_DOMAIN,
+            client_id: process.env.REACT_APP_AUTH0_CLIENT_ID
+          })
+          try {
+            const token = await auth0.getTokenSilently();
+            setToken(token)
+          } catch (err) {
+            console.log("token error", err)
+          }
+        }
+      }
+      fetchData()
+    }, [submitted, isAuthenticated]);
+
   
   const handleSubmit = (evt) => {
       evt.preventDefault();
       if (user.email_verified === false) {
         alert(user.email+' needs to be verified before placing order. Check your email for verification link.')
       } else {
-        alert(`${name}, your order has been placed..`)
+        setSubmitted(true)
+        console.log("access token", accessToken)
+        alert(`${salutation} ${lastName}, your order has been placed..`)
       }
   }
-  const {
-    user,
-    isAuthenticated,
-  } = useAuth0()
-  console.log("user", JSON.stringify(user))
+
+
+
   return (
     isAuthenticated && (
     <form onSubmit={handleSubmit}>
       <label>
-        Frirst Name:
+        Salutation:
+        <select onChange={e => { setSalutation(e.target.value)}}> 
+          <option value="Mr.">Mr.</option>
+          <option value="Mrs.">Mrs.</option>
+          <option value="Ms.">Ms.</option>
+          <option selected value="none">None</option>
+        </select>
+      </label>
+      <label>
+        First Name:
         <input
           type="text"
-          value={name}
+          value={firstName}
           onChange={e => {
-            setName(e.target.value)
+            setFirstName(e.target.value)
           }}
         />
       </label>
+      <label>
+        Last Name:
+        <input
+          type="text"
+          value={lastName}
+          onChange={e => {
+            setLastName(e.target.value)
+          }}
+        />
+      </label>
+      <label>
+        Phone Number:
+        <input
+          type="text"
+          value={phone}
+          onChange={e => {
+            setPhone(e.target.value)
+          }}
+        />
+      </label>
+
       <input type="submit" value="Submit" />
     </form>
   )

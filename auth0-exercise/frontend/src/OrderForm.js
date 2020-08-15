150504c54accba15a59c182ad147a92a31781c35
Gerdisch <github@jesslex.com>
2020-08-15 08:41:21 -0500

    pretty good working version

--- auth0-exercise/frontend/src/OrderForm.js
+++ auth0-exercise/frontend/src/OrderForm.js
@@ -14,31 +14,182 @@ import {
   Container, Form
 } from 'reactstrap';
 
+// Get domains, audiences, etc from environment variables
+const domain = process.env.REACT_APP_AUTH0_DOMAIN
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE
+const backendUrl = process.env.REACT_APP_BACKEND_URL
+
 // Build the order form and manage orders.
-export function OrderForm(props) {
-  // Form-related data
+const OrderForm = () => {
   const [firstName, setFirstName] = useState("First Name");
   const [lastName, setLastName] = useState("Last Name");
   const [phone, setPhone] = useState("630-555-1212");
   const [salutation, setSalutation] = useState("none");
-  const [orderInfo, setOrderInfo] = useState("");
-  const [submits, setSubmits] = useState(0);
-  // Token data for interacting with the backend API
-  //const [accessToken, setToken] = useState("");
+  const [submitted, setSubmitted] = useState(false)
 
-  // Get domains, audiences, etc from environment variables
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
-  const audience = process.env.REACT_APP_AUTH0_AUDIENCE
-  const backendUrl = process.env.REACT_APP_BACKEND_URL
+
+  //const {user, isAuthenticated} = useAuth0()
+  const {user,isAuthenticated} = useAuth0()
+
+  
+
+  // Process the form with any data available from the backend DB 
+  // The form is only available if the user was authenticated. 
+  useEffect(() => {
+    const processSubmit = async () => {
+        // using a free cors proxy
+        const corsProxy = "https://cors-anywhere.herokuapp.com/"
+        // build the base API URL with the cors proxy transversal
+        const apiUri = corsProxy+backendUrl+"/customer"
+      
+
+        if (submitted) {
+          setSubmitted(false); //reset
+          // Push user data to DB backend regardless
+          const payload = {
+            "email": user.email,
+            "lastName": lastName,
+            "firstName": firstName,
+            "phone": phone,
+            "salutation": salutation,
+          }
+          console.log("Payload", JSON.stringify(payload))
+          // Get a fresh token
+        const auth0 = await createAuth0Client({
+          domain: domain,
+          client_id: clientId
+        })
+        let token = ""
+        try {
+          token = await auth0.getTokenSilently({audience: audience});
+          console.log("awaited token", token)
+        } catch (err) {
+          console.log("token error", err)
+        }
+          const uri = apiUri
+            const user_fetch = await fetch(uri, {
+              method: 'POST',
+                headers: {
+                    'X-Requested-With': 'corsproxy', // can be any value - needed for cors proxy
+                    'Authorization': 'Bearer '+ token
+                  },
+                body: JSON.stringify(payload)
+            })
+            const user_data = await user_fetch.json()	
+            console.log("USER_DATA", JSON.stringify(user_data))
+          // Only process the pizza order if the user has verified their email
+          if (user.email_verified) {
+            alert(`${salutation} ${lastName}, your order has been placed..`)
+          } else {
+            alert(user.email+' needs to be verified before placing order. Check your email for verification link.')
+          }
+        }
+    }
+    processSubmit();
+  }); 
+
+  // Build the order form once the user has logged in.
+  return (
+    isAuthenticated && (
+    <Container className={styles.form}>
+    <Form onSubmit={event => {
+      setSubmitted(true);
+      event.preventDefault();
+    }}>
+      <label>
+        Salutation:
+        <select defaultValue={salutation} onBlur={e => { setSalutation(e.target.value)}}> 
+          <option value="Mr.">Mr.</option>
+          <option value="Mrs.">Mrs.</option>
+          <option value="Ms.">Ms.</option>
+          <option value="none">None</option>
+        </select>
+      </label>
+      <label>
+        First Name:
+        <input
+          type="text"
+          defaultValue={firstName}
+          onBlur={e => {
+            setFirstName(e.target.value)
+          }}
+        />
+      </label>
+      <label>
+        Last Name:
+        <input
+          type="text"
+          defaultValue={lastName}
+          onBlur={e => {
+            setLastName(e.target.value)
+          }}
+        />
+      </label>
+      <label>
+        Phone Number:
+        <input
+          type="text"
+          defaultValue={phone}
+          onBlur={e => {
+            setPhone(e.target.value)
+          }}
+        />
+      </label>
+
+      <input type="submit" value="Submit" />
+    </Form>
+    </Container>
+  )
+  )
+}
+
+export default OrderForm;
+/**** /
+         // If first time in, get the current data for the user from the backend DB
+        if ((typeof(user) !== 'undefined') && (submits === 0)) {
+            const uri = apiUri+"?email="+user.email
+            const user_fetch = await fetch(uri, {
+              method: 'GET',
+                headers: {
+                    'X-Requested-With': 'corsproxy', // can be any value - needed for cors proxy
+                    'Authorization': 'Bearer '+ token
+                  }
+            })
+            const user_data = await user_fetch.json()	
+            console.log("USER_DATA", JSON.stringify(user_data))
+            if (user_data) {
+              setFirstName(user_data.firstName)
+              setLastName(user_data.lastName)
+              setPhone(user_data.phone)
+              setSalutation(user_data.salutation)
+            } 
+        } else if (submits > 0) { 
+
+
+*/
+
+
+
   //const serviceUrl = process.env.REACT_APP_SERVICE_URL
-
-  // using a free cors proxy
-  const corsProxy = "https://cors-anywhere.herokuapp.com/"
-  // build the base API URL with the cors proxy transversal
-  const apiUri = corsProxy+backendUrl+"/customer"
-
-  const {user, isAuthenticated} = useAuth0()
+//,[submits, firstName, lastName, phone, salutation, domain, audience, clientId, apiUri, user]); // the submitted value is used as a flag 
+  ///return("nothingrightnow")
+////}
+  /***** 
+  // Processes the form when the user hits submit.
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("handleSubmit",JSON.stringify(evt))
+    setOrderInfo("goo")
+    if (user.email_verified === false) {
+      alert(user.email+' needs to be verified before placing order. Check your email for verification link.')
+    } else {
+      const newsub = submits+1
+      setSubmits(newsub) //keep track of number of submissions
+      const address = (salutation ? salutation : "");
+      alert(`${address} ${lastName}, your order has been placed..`)
+    }
+  }
 
   /****** 
   // Should be run when the page is first set
@@ -67,148 +218,3 @@ export function OrderForm(props) {
     console.log("getToken useEffect done");
   }, [isAuthenticated, domain, clientId, audience]); 
   *******/
-
-  // Process the form with any data available from the backend DB 
-  // The form is only available if the user was authenticated. 
-  useEffect(() => {
-    const processSubmit = async () => {
-      /*
-      setFirstName("First Name")
-      setLastName("Last Name")
-      setPhone("Phone Number")
-      setSalutation("none")
-      */
-
-      // Get a fresh token
-        const auth0 = await createAuth0Client({
-          domain: domain,
-          client_id: clientId
-        })
-        let token = ""
-        try {
-          token = await auth0.getTokenSilently({audience: audience});
-          console.log("awaited token", token)
-          //setToken(token)
-        } catch (err) {
-          console.log("token error", err)
-        }
-
-        // If first time in, get the current data for the user from the backend DB
-        if ((typeof(user) !== 'undefined') && (submits === 0)) {
-            const uri = apiUri+"?email="+user.email
-            const user_fetch = await fetch(uri, {
-              method: 'GET',
-                headers: {
-                    'X-Requested-With': 'corsproxy', // can be any value - needed for cors proxy
-                    'Authorization': 'Bearer '+ token
-                  }
-            })
-            const user_data = await user_fetch.json()	
-            console.log("USER_DATA", JSON.stringify(user_data))
-            if (user_data) {
-              setFirstName(user_data.firstName)
-              setLastName(user_data.lastName)
-              setPhone(user_data.phone)
-              setSalutation(user_data.salutation)
-            } 
-        } else if (submits > 0) {
-          // Push data to DB backend
-          const body = {
-            "email": user.email,
-            "lastName": orderInfo.lastName,
-            "firstName": orderInfo.firstName,
-            "phone": orderInfo.phone,
-            "salutation": orderInfo.salutation,
-          }
-          console.log("Body", JSON.stringify(body))
-          const uri = apiUri
-            const user_fetch = await fetch(uri, {
-              method: 'POST',
-                headers: {
-                    'X-Requested-With': 'corsproxy', // can be any value - needed for cors proxy
-                    'Authorization': 'Bearer '+ token
-                  },
-                body: JSON.stringify(body)
-            })
-            const user_data = await user_fetch.json()	
-            console.log("USER_DATA", JSON.stringify(user_data))
-        }
-    }
-    processSubmit();
-  }, [submits,orderInfo,apiUri,audience,clientId,domain,user]); //,[submits, firstName, lastName, phone, salutation, domain, audience, clientId, apiUri, user]); // the submitted value is used as a flag 
-
-  // Processes the form when the user hits submit.
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    console.log("handleSubmit",JSON.stringify(evt))
-    setOrderInfo("goo")
-    if (user.email_verified === false) {
-      alert(user.email+' needs to be verified before placing order. Check your email for verification link.')
-    } else {
-      const newsub = submits+1
-      setSubmits(newsub) //keep track of number of submissions
-      const address = (salutation ? salutation : "");
-      alert(`${address} ${lastName}, your order has been placed..`)
-    }
-  }
-
-  // Build the order form once the user has logged in.
-  return (
-    isAuthenticated && (
-    <Container className={styles.form}>
-    <Form onSubmit={()=>
-      setOrderInfo({
-        firstNane: firstName,
-        lastName: lastName,
-        phone: phone,
-        salutation: salutation
-      })
-    }>
-      <label>
-        Salutation:
-        <select defaultValue={salutation} onChange={e => { setSalutation(e.target.value)}}> 
-          <option value="Mr.">Mr.</option>
-          <option value="Mrs.">Mrs.</option>
-          <option value="Ms.">Ms.</option>
-          <option value="none">None</option>
-        </select>
-      </label>
-      <label>
-        First Name:
-        <input
-          type="text"
-          value={firstName}
-          onChange={e => {
-            setFirstName(e.target.value)
-          }}
-        />
-      </label>
-      <label>
-        Last Name:
-        <input
-          type="text"
-          value={lastName}
-          onChange={e => {
-            setLastName(e.target.value)
-          }}
-        />
-      </label>
-      <label>
-        Phone Number:
-        <input
-          type="text"
-          value={phone}
-          onChange={e => {
-            setPhone(e.target.value)
-          }}
-        />
-      </label>
-
-      <input type="submit" value="Submit" />
-    </Form>
-    </Container>
-  )
-  )
-}
-
-export default OrderForm;
